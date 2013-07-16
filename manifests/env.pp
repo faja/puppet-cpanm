7e73a11a739f08b07099b5205a0a70375b9689b8
Marcin Cabaj <mcabaj@gmail.com>
2013-07-16 15:39:01 +0000

    Update env.pp

--- manifests/env.pp
+++ manifests/env.pp
@@ -8,43 +8,41 @@ define cpanm::env (
   if $user == 'root' and $locallib == true {
     file {"/root/test":
       ensure => present,
-	  content => "if\n",
+      content => "if\n",
     }
-
   }
+  
   elsif $user == 'root' {
     file {"/root/test":
       ensure => present,
-	  content => "else\n",
+      content => "else\n",
     }
   }
+  
   else {
     file {"$homedir/bin":
       ensure  => directory,
-	  owner   => $user,
-	  group   => $user,
-	} ->
-	exec {"$user-cpanm-bin":
-	  command => "curl -L http://cpanmin.us > $homedir/bin/cpanm",
-	  unless  => "ls $homedir/bin/cpanm",
-	  user    => $user,
-	  path    => ['/bin','/usr/bin/']
-	} ->
-	file {"$homedir/bin/cpanm":
-	  ensure => present,
-	  mode   => 0744,
+      owner   => $user,
+      group   => $user,
     } ->
-
+    exec {"$user-cpanm-bin":
+      command => "curl -L http://cpanmin.us > $homedir/bin/cpanm",
+      unless  => "ls $homedir/bin/cpanm",
+      user    => $user,
+      path    => ['/bin','/usr/bin/']
+    } ->
+    file {"$homedir/bin/cpanm":
+      ensure => present,
+      mode   => 0744,
+    } ->
     exec {"$user-cpanm-local-lib":
-	  command => "su - $user -c \"cpanm --local-lib=$homedir/perl5 local::lib\"",
-	  unless  => "ls $homedir/perl5/lib/perl5/local/lib.pm",
-	  path    => ["$homedir/bin",'/bin','/usr/bin/']
-	} ->
-
-	file_line {"cpanm_in_bashrc":
-	  path => "$homedir/.bashrc",
-	  line => "eval $(perl -I\$HOME/perl5/lib/perl5 -Mlocal::lib)",
-	}
-	  
+      command => "su - $user -c \"cpanm --local-lib=$homedir/perl5 local::lib\"",
+      unless  => "ls $homedir/perl5/lib/perl5/local/lib.pm",
+      path    => ["$homedir/bin",'/bin','/usr/bin/']
+    } ->
+    file_line {"cpanm_in_bashrc":
+      path => "$homedir/.bashrc",
+      line => "eval $(perl -I\$HOME/perl5/lib/perl5 -Mlocal::lib)",
+    }
   }
 }
