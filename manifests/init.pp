940e803bdea1bd63ada523765d8da5e4142b2548
faja <mcabaj@gmail.com>
2013-07-17 12:26:56 +0000

    add redhat/centos and ubuntu/debian compatibility

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,24 @@
 class cpanm {
-  
+ 
+  $compatible = ['Debian','Ubuntu','CentOS','RedHat']
+  if ! ($::operatingsystem in $compatible) {
+    fail("Module is not compatible with ${::operatingsystem}")
+  }
+
   if ! defined(Package['perl'])       { package { 'perl':       ensure => installed } }
-  if ! defined(Package['perl-devel']) { package { 'perl-devel': ensure => installed } }
-  if ! defined(Package['perl-CPAN'])  { package { 'perl-CPAN':  ensure => installed } }
   if ! defined(Package['curl'])       { package { 'curl':       ensure => installed } }
   if ! defined(Package['wget'])       { package { 'wget':       ensure => installed } }
+
+
+  case $operatingsystem {
+    'RedHat', 'CentOS': { 
+      if ! defined(Package['perl-devel']) { package { 'perl-devel': ensure => installed } }
+      if ! defined(Package['perl-CPAN'])  { package { 'perl-CPAN':  ensure => installed } }
+    }
+    'Debian', 'Ubuntu': {
+      if ! defined(Package['perl-base'])     { package { 'perl-base':     ensure => installed } }
+      if ! defined(Package['perl-modules'])  { package { 'perl-modules':  ensure => installed } }
+    }
+  }
 
 }
